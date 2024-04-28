4be6fd19b9108aa4b3a2a219a7925596872a4ffd
pRodriguez1630 <prodriguez1630@gmail.com>
2024-04-28 14:41:59 -0400

    second-test-v3

--- main.tf
+++ main.tf
@@ -23,6 +23,11 @@ resource "google_storage_bucket" "mi_bucket-v" {
   location      = var.region  # Cambia esto a tu región preferida
   force_destroy = true           # Esto eliminará permanentemente el bucket cuando sea eliminado de Terraform
 }
+resource "google_storage_bucket" "mi_bucket-v2" {
+  name          = "mi-bucket-de-logs-v"
+  location      = var.region  # Cambia esto a tu región preferida
+  force_destroy = true           # Esto eliminará permanentemente el bucket cuando sea eliminado de Terraform
+}
 
 # Crear un trigger de Cloud Build y utilizar el bucket creado anteriormente
 # resource "google_cloudbuild_trigger" "mi_trigger" {
